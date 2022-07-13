64dd85a6e075db9c97476f1d0226ed3d2899e454
ashishsangra <109223916+ashishsangra@users.noreply.github.com>
2022-07-13 19:16:32 +0530

    made-change-in-awk-command-by-ashish-sangra-choose-column5-calculate-average-response-time

--- latte.sh
+++ latte.sh
@@ -10,4 +10,4 @@ while [[ $i -le $j ]]
 do
  echo -n "Probe-$i :: ";curl -w 'Local IP: %{local_ip}; Host: %{remote_ip}; Return HTTP Code: %{http_code}; Bytes received: %{size_download}; Response time: %{time_total}; Download Speed: %{speed_download}\n' $1 -m 2 -o /dev/null -s 
  i=$(($i+1)) 
- done | tee /dev/tty | awk '{ sum += $NF; n++ } END { if (n > 0) print "Average Resp Time=",sum / n; }'
+ done | tee /dev/tty | awk '{ sum += $5; n++ } END { if (n > 0) print "Average Resp Time=",sum / n; }'
