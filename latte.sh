02fa7879e456944d4abe2d275e1c1251da59b268
ashishsangra <109223916+ashishsangra@users.noreply.github.com>
2022-07-13 20:15:40 +0530

    Update latte.sh

--- latte.sh
+++ latte.sh
@@ -2,12 +2,11 @@ echo -e "\n"
 echo -e "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++DISCLAIMER+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 echo -e "Crafted by @ashishsangra@microsoft.com\n"
 echo -e "Starting Test\n"
-echo -n "Number of iterations: "
-read j
+read -p "Number of iterations: " j
 i=1
 
 while [[ $i -le $j ]] 
 do
  echo -n "Probe-$i :: ";curl -w 'Local IP: %{local_ip}; Host: %{remote_ip}; Return HTTP Code: %{http_code}; Bytes received: %{size_download}; Response time: %{time_total}; Download Speed: %{speed_download}\n' $1 -m 2 -o /dev/null -s 
  i=$(($i+1)) 
- done | awk '{print $15}' | tr -d ';' | awk '{sum+=$15} END {print "Average response time=",sum}'
+ done | awk '{print $15}' | tr -d ';' | awk '{sum+=$1} END {print "Average response time=",sum}'
