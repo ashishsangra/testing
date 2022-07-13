ffe84741521723d47ff5e9fcd847d24ce74cb023
ashishsangra <109223916+ashishsangra@users.noreply.github.com>
2022-07-13 20:57:13 +0530

    Update latte.sh
    deleted unused lines

--- latte.sh
+++ latte.sh
@@ -9,6 +9,4 @@ while [[ $i -le $j ]]
 do
  echo -n "Probe-$i :: ";curl -w 'Local IP: %{local_ip}; Host: %{remote_ip}; Return HTTP Code: %{http_code}; Bytes received: %{size_download}; Response time: %{time_total}; Download Speed: %{speed_download}\n' $1 -m 2 -o /dev/null -s
   i=$(($i+1))
-   done | tee /dev/tty | awk '{print $17}' | awk '{sum+=$1;}END{print "Average response time=",sum}'
-                                                                                                                                                                                       ✔
-
+   done | tee /dev/tty | awk '{print $17}' | awk '{sum+=$1;}END{print "Average response time=",sum}' && echo "Tests Ran succesfully"   
