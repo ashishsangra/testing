561873512329f2bef83eb9c52050a25e98a11736
ashishsangra <109223916+ashishsangra@users.noreply.github.com>
2022-07-13 21:07:21 +0530

    Update latte.sh
    final script! Success !

--- latte.sh
+++ latte.sh
@@ -8,5 +8,5 @@ i=1
 while [[ $i -le $j ]]
 do
  echo -n "Probe-$i :: ";curl -w 'Local IP: %{local_ip}; Host: %{remote_ip}; Return HTTP Code: %{http_code}; Bytes received: %{size_download}; Response time: %{time_total}; Download Speed: %{speed_download}\n' $1 -m 2 -o /dev/null -s
-  i=$(($i+1))
-   done | tee /dev/tty | awk '{print $17}' | awk '{sum+=$1;}END{print "Average response time=",sum}' && echo "Tests Ran succesfully"   
+   i=$(($i+1))
+      done | tee /dev/tty | awk '{print $17}' | awk '{sum+=$1;} END { if (NR > 0) print "Average response time=",sum / NR}' && echo -e "\n" && echo "Tests Ran succesfully"
