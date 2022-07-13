e152704bc934efc33ef05a070324e7379b6edaaf
ashishsangra <109223916+ashishsangra@users.noreply.github.com>
2022-07-13 20:13:41 +0530

    Update latte.sh
    removed /dev/tty, don't open tty and copy the last printed column, copy 15th column from the curl ouput to calculate average responsetie

--- latte.sh
+++ latte.sh
@@ -10,4 +10,4 @@ while [[ $i -le $j ]]
 do
  echo -n "Probe-$i :: ";curl -w 'Local IP: %{local_ip}; Host: %{remote_ip}; Return HTTP Code: %{http_code}; Bytes received: %{size_download}; Response time: %{time_total}; Download Speed: %{speed_download}\n' $1 -m 2 -o /dev/null -s 
  i=$(($i+1)) 
- done | tee /dev/tty | awk '{print $15}' | tr -d ';' | awk '{sum+=$15} END {print "Average response time=",sum}'
+ done | awk '{print $15}' | tr -d ';' | awk '{sum+=$15} END {print "Average response time=",sum}'
